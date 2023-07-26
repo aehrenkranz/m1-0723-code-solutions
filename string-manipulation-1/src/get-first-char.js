f6cab9d0522f45bd8095d3a5fcafd8b567c0c413
Alex Ehrenkranz <alex.ehrenkranz+git@gmail.com>
2023-07-26 03:53:47 +0000

    complete string-manipulation-1

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,8 @@
+/* exported getFirstChar */
+/**
+ * Define a function named getFirstChar, with one parameter, 'string'
+ * *Return the value of the first index location of the parameter 'string'
+ */
+function getFirstChar(string) {
+  return string[0];
+}
